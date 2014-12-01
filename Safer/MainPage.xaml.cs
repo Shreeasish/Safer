1af5d4a1cd0a147dea8c8312014bb65567e4169f
Shreeasish Kumar <shreeasish@live.com>
2014-12-02 00:54:17 +0530

    Added function to request permission Awaiting stackoverflow http://stackoverflow.com/questions/27232439/multiple-triggers-for-a-backgroundtask-windows-phone-8-1

--- Safer/MainPage.xaml.cs
+++ Safer/MainPage.xaml.cs
@@ -12,7 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Sensors;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
 namespace Shreeasish.Safer
@@ -22,13 +23,116 @@ namespace Shreeasish.Safer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+    #region registration objects created
+        private Accelerometer _acclerometer;
+       
+
+        private DeviceUseTrigger accelerometerTrigger;
+
+        private string error;
+
+        public bool BackgroundRequestGranted= false;
+
+        private BackgroundTaskRegistration saferAccelerometerRegistration;
+        #endregion
+
+        /// <summary>
+        /// Constructor for MainPage
+        /// </summary>
         public MainPage()
         {
             this.InitializeComponent();
 
+            _acclerometer = Accelerometer.GetDefault();
+            
+
+            #region Accelerometer Registration
+            
+            #endregion
             this.NavigationCacheMode = NavigationCacheMode.Required;
+        }//Constructor End
+
+
+        /// <summary>
+        /// Function to Request Background Access, Sets BackgroundRequestGranted to 'true' if granted
+        /// Also checks if the accelerometer exists
+        /// </summary>
+        async void RequestBackgroundAccessAsync()
+        {
+            if (_acclerometer != null)
+            {
+
+                accelerometerTrigger = new DeviceUseTrigger();
+
+                BackgroundAccessStatus accessStatus = await BackgroundExecutionManager.RequestAccessAsync();
+
+                if ((BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity == accessStatus) ||
+                    (BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity == accessStatus))
+                {
+                    BackgroundRequestGranted = true;
+                    return;
+                }
+                else
+                {
+#warning shift to UI
+
+                    error = "Background tasks may be disabled for this app";
+                    return;
+                }
+
+            }
+            else
+            {
+#warning shift to UI
+                
+                error = "Accelerometer unavailable";
+                return;
+            }
+        }//RequestBackgroundAccess end
+
+
+        /// <summary>
+        /// Finds a previously registered background task for this scenario and cancels it (if present)
+        /// </summary>
+        private void FindAndCancelExistingBackgroundTask()
+        {
+            foreach (var backgroundTask in BackgroundTaskRegistration.AllTasks.Values)
+            {
+                
+                if (SaferConfiguration.GetTaskName() == backgroundTask.Name)
+                {
+                    ((BackgroundTaskRegistration)backgroundTask).Unregister(true);
+                    break;
+                }
+            }
         }
 
+        
+
+        public bool RegisterBackgroundTask()
+        {
+            FindAndCancelExistingBackgroundTask();
+            BackgroundTaskBuilder accelerometerTaskBuilder = new BackgroundTaskBuilder()
+            {
+                Name = SaferConfiguration.GetTaskName(),
+                TaskEntryPoint = SaferConfiguration.GetTaskEntryPoint(),
+            };
+            
+            accelerometerTaskBuilder
+
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         /// <summary>
         /// Invoked when this page is about to be displayed in a Frame.
         /// </summary>
@@ -36,6 +140,10 @@ namespace Shreeasish.Safer
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+
+            RequestBackgroundAccessAsync();
+            
+
             // TODO: Prepare page for display here.
 
             // TODO: If your application contains multiple pages, ensure that you are
@@ -43,6 +151,21 @@ namespace Shreeasish.Safer
             // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
+
+
+
+
         }
-    }
+
+        private void AppBarToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        
+
+
+    }//Class End
 }
---
+++ Safer/SaferConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shreeasish.Safer
+{
+    class SaferConfiguration
+    {
+        private static const string AccelerometerTaskName = "AccelerometerTask";
+       
+        private static const string AccelerometerTaskEntryPoint = "SaferBackGroundTasks.AccelerometerTask";
+        /// <summary>
+        ///Returns the Accelerometer Task Name
+        /// </summary>
+        /// <returns>(String)</returns>
+        public static string GetTaskName()
+        {
+            return AccelerometerTaskName;
+        }
+        /// <summary>
+        /// Returns the Accelerometer Task Entry Point
+        /// </summary>
+        /// <returns></returns>
+        public static string GetTaskEntryPoint()
+        {
+            return AccelerometerTaskEntryPoint;
+        }
+	
+
+    }
+}
